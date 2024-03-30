03f7bfde5f46450f6a7a5e0cc90f7564048fb69c
MsDiala <diala-is-diala@hotmail.com>
2024-03-30 22:26:14 +0000

    d

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Write your code here
+
+// Create a class for Breakfast
+class Breakfast {
+    constructor(food, drink) {
+      this.food = food;
+      this.drink = drink;
+    }
+  }
+  
+  // Create a class for Lunch
+  class Lunch {
+    constructor(salad, soup, drink) {
+      this.salad = salad;
+      this.soup = soup;
+      this.drink = drink;
+    }
+  }
+  
+// Create a class for Dinner
+class Dinner {
+    constructor(salad, soup, entree, dessert) {
+      this.salad = salad;
+      this.soup = soup;
+      this.entree = entree;
+      this.dessert = dessert; 
+    }
+  }
+  
+  
+  // Export the classes
+  module.exports = {
+    Breakfast,
+    Lunch,
+    Dinner
+  };
+  
